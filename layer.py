5e1593987018dc13f0b1b289a36ad9d45c71c2a7
Ari-Pekka Honkanen <honkanen.ap@gmail.com>
2016-06-01 01:59:30 +0300

    Improved the speed oftraining function

--- layer.py
+++ layer.py
@@ -1,43 +1,44 @@
 from __future__ import division
-import numpy as np
+from numpy import exp, isnan, zeros, array, reshape,concatenate,dot
+from numpy.random import normal
 
 
 def sigmoid(x):
-    x = 1/(1+np.exp(-x))
-    x[np.isnan(x)] = 0
+    x = 1/(1+exp(-x))
+    x[isnan(x)] = 0
     return x
 
 class Layer:
     def __init__(self,n_input):
 
         self.n_input = n_input
-        self.weight_matrix = np.zeros((0,n_input))
-        self.bias_vector = np.zeros((0,1))
+        self.weight_matrix = zeros((0,n_input))
+        self.bias_vector = zeros((0,1))
         
     def add_neuron(self,weights=None,bias=None):
         
         if not weights == None:
             #reshape to 1d array
-            weights = np.array(weights)
-            weights = np.reshape(weights,(1,-1))
+            weights = array(weights)
+            weights = reshape(weights,(1,-1))
         else:
-            weights = np.random.normal(size = (1,self.n_input))
+            weights = normal(size = (1,self.n_input))
 
         if not bias == None:            
-            bias = np.array(bias)
-            bias = np.reshape(bias,(1,1))
+            bias = array(bias)
+            bias = reshape(bias,(1,1))
         else:
-            bias = np.random.normal(size = (1, 1))
+            bias = normal(size = (1, 1))
         
         #add neuron (i.e. its weights and bias to the appropriate matrices)
-        self.weight_matrix = np.concatenate((self.weight_matrix, weights))
-        self.bias_vector = np.concatenate((self.bias_vector, bias))
+        self.weight_matrix = concatenate((self.weight_matrix, weights))
+        self.bias_vector = concatenate((self.bias_vector, bias))
       
     def n_neurons(self):
         return self.weight_matrix.shape[0]
         
     def compute_output(self,input_vector):      
-        return sigmoid(np.dot(self.weight_matrix, input_vector) + self.bias_vector)
+        return sigmoid(dot(self.weight_matrix, input_vector) + self.bias_vector)
 
     def set_neuron(self,n,weights,bias):
         self.weight_matrix[n,:] = weights
